4140d1180e7de4e55db08f1fc41cfda9dfc5da15
Seishiro0922 <c4iikgaitf@i.softbank.jp>
2020-04-29 18:09:20 +0900

    master

--- k02/k02.c
+++ k02/k02.c
@@ -7,11 +7,13 @@ extern double p_stdnorm(double z);
 
 int main(void)
 {
-    double val;
+    int i;
+    double val[i],z1,z2,u1,b1,u2,b2,x1,x2,max_val=1,min_val=1;
     char fname[FILENAME_MAX];
     char buf[256];
     FILE* fp;
     double L1=1,L2=1;
+    
 
     printf("input the filename of sample:");
     fgets(fname,sizeof(fname),stdin);
@@ -26,9 +28,14 @@ int main(void)
 
     while(fgets(buf,sizeof(buf),fp) != NULL){
         sscanf(buf,"%lf",&val);
-
-
-    
+        for(i=0; ;i++){
+        z1=(val[i]-u1)/b1;
+        z2=(val[i]-u2)/b2;
+        x1=p_stdnorm(z1);
+        x2=p_stdnorm(z2);
+        max_val=max_val*x1;
+        min_val=min_val*x2;
+        }
 
 
 
