42949f7538da9148f4606d8466598ce847225c78
Seishiro0922 <c4iikgaitf@i.softbank.jp>
2020-05-19 13:30:15 +0900

    master

--- k03/k03.c
+++ k03/k03.c
@@ -19,12 +19,12 @@ int main(int argc, char* argv[])
     double sigma;
     int i;
     double dummy;
-    char str[4];
+    char n;
 
 
 
 
-    sscanf(str,"%s %s %s",mu,sigma,num_dummy);
+    sscanf(argv[4],"%s %s %s %s",n,&mu,&sigma,&num_dummy);
     printf("============================================\n");
     printf("template mean: %4.1lf\n",mu);
     printf("template standard deviation: %3.2lf\n",sigma);
